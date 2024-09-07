3f11ebf312050c7831511542cbb8b57d27e890f0
nichol6s <nicholas.jake15@gmail.com>
2024-09-07 12:39:02 -0300

    Fixing some screen bugs

--- app/src/main/java/com/example/cp1_kotlin/UserScreen.kt
+++ app/src/main/java/com/example/cp1_kotlin/UserScreen.kt
@@ -1,6 +1,7 @@
 package com.example.cp1_kotlin
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,7 +9,8 @@ import androidx.core.view.WindowInsetsCompat
 
 class UserScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         enableEdgeToEdge()
         setContentView(R.layout.activity_user_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
